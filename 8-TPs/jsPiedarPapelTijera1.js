e2d577fce6459639ea8747cda1f3c9970d324533
fsaibene <fernandomsaibene@hotmail.com>
2016-08-28 13:08:40 -0300

    punto 7 ok

--- 8-TPs/jsPiedarPapelTijera1.js
+++ 8-TPs/jsPiedarPapelTijera1.js
@@ -4,26 +4,68 @@ siendo 1 para “piedra”, el 2 para “papel” y 3 para “tijera”.
 El jugador seleccionará una imagen correspondiente 
 a su opción  y le informaremos si ganó, empató o perdió.
 */
-var eleccionMaquina;
+var app = angular.module("PPT1", []);
 
-function comenzar()
-{
-	
+app.controller("controladorPPT1", function ($scope) {
 
+    var maq;
+    $scope.comenzar= function () {
+        $scope.eleccionMaquina = Math.floor(Math.random() * 3)+1;
+        maq=  $scope.eleccionMaquina;
+    };
+    function Perder() {
+        alert("Perdio!");
+    }
+    function Ganar() {
+        alert("Gano!");
+    }
+    function Empatar() {
+        alert("Empato!");
+    }
 
-}//FIN DE LA FUNCIÓN
-function piedra()
-{
-	
+    $scope.piedra = function () {
+        console.log(maq);
+      switch (maq)
+      {
+          case 1:
+              Empatar();
+              break;
+          case 2:
+              Perder();
+              break;
+          case 3:
+              Ganar();
+      }
+        $scope.comenzar();
+    };
+    $scope.papel = function () {
+      switch (maq)
+      {
+          case 1:
+              Ganar();
+              break;
+          case 2:
+              Empatar();
+              break;
+          case 3:
+              Perder();
+      }
+        $scope.comenzar();
 
-}//FIN DE LA FUNCIÓN
-function papel()
-{
+    };
+        $scope.tijera = function () {
+          switch (maq)
+          {
+              case 1:
+                  Perder();
+                  break;
+              case 2:
+                  Ganar();
+                  break;
+              case 3:
+                  Empatar();
+          }
+            $scope.comenzar();
+        }
 
-
-}//FIN DE LA FUNCIÓN
-function tijera()
-{
-	
-
-}//FIN DE LA FUNCIÓN
+});
