52c17f2702fd0420909b7a448d11657e163f8eff
fsaibene <fernandomsaibene@hotmail.com>
2016-08-27 21:19:38 -0300

    punto 4 ok

--- 8-TPs/jsFerreteIluminacion.js
+++ 8-TPs/jsFerreteIluminacion.js
@@ -7,8 +7,52 @@ D.	Si compra 3  lamparitas bajo consumo marca "ArgentinaLuz"  el descuento es de
 E.	Si el importe final con descuento suma más de $120  se debe sumar un 10% de ingresos brutos en informar del impuesto con el siguiente mensaje:
  ”Usted pago X de IIBB.”, siendo X el impuesto que se pagó. 
 
- */
-function CalcularPrecio () 
-{
- 	
-}
+ */var app = angular.module("FerreteIluminacion", []);
+
+app.controller("controladorIluminacion", function ($scope) {
+    $scope.CalcularPrecio=function () {
+        var rdo;
+        var bruto;
+        $scope.msj='';
+        bruto=35*$scope.ingreso;
+        if($scope.ingreso >= 6)
+        {
+            rdo= bruto*0.90;
+        }
+        if($scope.ingreso == 5)
+        {
+            if($scope.opcion== "ArgentinaLuz")
+            {
+                rdo=bruto*0.60;
+            }else
+                rdo=bruto*0.70;
+        }
+        if($scope.ingreso == 4)
+        {
+            if($scope.opcion== "ArgentinaLuz" || $scope.opcion== "FelipeLamparas")
+            {
+                rdo=bruto*0.75;
+            }else
+                rdo=bruto*0.80;
+
+        }
+        if($scope.ingreso == 3)
+        {
+            if($scope.opcion== "ArgentinaLuz" ){
+                rdo=bruto*0.85;
+            }else if($scope.opcion== "FelipeLamparas") {
+                rdo = bruto * 0.10;
+            }else
+                rdo=bruto*0.95;
+        }
+        if(rdo>120)
+        {
+            var imp=rdo*0.10;
+            rdo=rdo+imp;
+            $scope.msj='Usted pago $'+imp+' de IIBB';
+        }
+        $scope.resultado=Math.round(rdo);
+    }
+
+
+});
