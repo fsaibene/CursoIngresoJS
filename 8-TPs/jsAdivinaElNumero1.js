43ccb4a31d44262c92a2550031cffc6ea7814734
fsaibene <fernandomsaibene@hotmail.com>
2016-08-28 11:42:27 -0300

    Punto 5 Adivina el nro1

--- 8-TPs/jsAdivinaElNumero1.js
+++ 8-TPs/jsAdivinaElNumero1.js
@@ -7,20 +7,23 @@ secreto del 1 al 100, en la pantalla del juego
 “Usted es un ganador!!! y en solo X intentos”.
 de no ser igual se debe informar si “falta…”  para llegar al número secreto  o si “se pasó…”  del número secreto.
 */
-var numeroSecreto; 
-var contadorIntentos;
 
-function comenzar()
-{
-	//Genero el número RANDOM entre 1 y 100
-	 
-		//alert(numeroSecreto );
-	
+var app = angular.module('AdivinaElNumero1', []);
 
-}
+app.controller("controladorAdivina1", function ($scope) {
+    $scope.comenzar = function () {
+        $scope.contadorIntentos = 0;
+        $scope.numeroSecreto= Math.floor(Math.random() * 100) + 1 ;
+    };
 
-function verificar()
-{
-	
-	
-}
+    $scope.verificar = function ()
+    {
+        if($scope.ingreso == $scope.numeroSecreto) {
+            alert("Usted es un ganador!!! y en solo " + $scope.contadorIntentos + " intentos");
+            $scope.contadorIntentos=0;
+        }else
+            $scope.contadorIntentos++;
+
+    }
+});
+
